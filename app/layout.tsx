4a80101d0c6de1c5c7b6ff31069ea57cd528fad5
GreyFox00001 <reachtanish@gmail.com>
2026-03-21 00:42:30 +0530

    togglebuttonadd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,22 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/font';
+import { ThemeProvider } from './components/theme-provider';
+import { ThemeToggle } from './ui/ToggleButton';
 
 export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider 
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <ThemeToggle/>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
